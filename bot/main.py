e813dc1a309835609f5cd520c7ded907e7e8bdde
Giovanni <134148500+GiovanniPeirone@users.noreply.github.com>
2024-07-02 12:51:56 -0300

    Create main.py

---
+++ bot/main.py
@@ -0,0 +1,32 @@
+
+
+def menu1():
+    menu = """
+        +---------------------------------------------+
+        |            1-  * Comenzar                   |
+        |            2-  *    Salir                   |
+        +---------------------------------------------+
+    """
+
+    o = int(input(menu))
+    if o == 2:
+        return False
+    elif 0 == 1:
+        return True
+#-------------------
+
+def menu2():
+    
+
+#-------------------
+main = menu1()
+while main:
+    try:
+        direccion = str(input("direccion:"))
+        resolucion = str(input("resolcion:"))
+
+
+
+    except ValueError:
+        break
+
